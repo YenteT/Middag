86c9d90d49565b82b97f514343d869a3e3780a1a
YenteT <spamytspam@gmail.com>
2020-03-06 11:08:45 +0100

    Upload Vorderingen vrijdag 6 maart 2020:
    Project gemaakt

---
+++ TestProject/TestProject/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using TestProject.Models;
+
+namespace TestProject.Controllers
+{
+    public class HomeController : Controller
+    {
+
+    }
+}
